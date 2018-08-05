d7ff5b8afca92845e5274a2650d72558b7895cbb
DanielChen1234 <dchen8492@yahoo.com>
2018-08-05 14:33:49 -0400

    maps working

--- client/components/Map.js
+++ client/components/Map.js
@@ -12,7 +12,7 @@ class Map extends Component {
    return(
       <div>
         <GoogleMapExample
-          containerElement={ <div style={{ height: `500px`, width: '500px' }} /> }
+          containerElement={ <div style={{ height: `300px`, width: '300px' }} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
         />
       </div>
